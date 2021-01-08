6ced6fa3c7217e61790e73f4a8dc505ab2030b3c
AETSRound <qkrcksdna@naver.com>
2021-01-08 17:59:00 +0900

    Please enter the commit message for your changes. Lines starting

--- stripcomments.js
+++ stripcomments.js
@@ -1,18 +1,23 @@
 function solution(strings, arr){
     let temp = [];
     let copy = strings.split('\n');
-    console.log(copy);
+    // console.log(copy);
     for(let mark of arr){
         // console.log(copy);
-        for(let c of copy){
+        for(let i in copy){
+            let c = copy[i];
             let end = c.indexOf(mark);
+            console.log("====")
+            console.log(temp)
+            console.log("====")
             if(end === -1){
                 temp.push(c);
             }else{
                 temp.push(c.substr(0, end));
             }
+            copy[i] = c;
         }
-        
+       
     }
     console.log(temp);
 }
