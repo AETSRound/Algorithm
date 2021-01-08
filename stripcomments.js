0603b70ef94c5f2d22695dd3bd927dbef2466e3b
AETSRound <qkrcksdna@naver.com>
2021-01-08 17:55:03 +0900

    진행중

--- stripcomments.js
+++ stripcomments.js
@@ -1,3 +1,20 @@
-function stripComent(strings){
-    
+function solution(strings, arr){
+    let temp = [];
+    let copy = strings.split('\n');
+    console.log(copy);
+    for(let mark of arr){
+        // console.log(copy);
+        for(let c of copy){
+            let end = c.indexOf(mark);
+            if(end === -1){
+                temp.push(c);
+            }else{
+                temp.push(c.substr(0, end));
+            }
+        }
+        
+    }
+    console.log(temp);
 }
+
+solution("apples, pears # and bananas\ngrapes\nbananas !apples", ["#", "!"])
