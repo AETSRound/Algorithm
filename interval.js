d50ec3013b076a102f739916bb24ad0986622ecb
AETSRound <qkrcksdna@naver.com>
2021-01-08 14:14:46 +0900

    잠시 중지 어려움...

---
+++ interval.js
@@ -0,0 +1,21 @@
+function sumIntervals(arrs){
+    let count= 0;
+    let isInclude = false;
+    let isCommon = false;
+    let temp = [];
+    for(let arr of arrs){
+        temp.push([arr[0], arr[1]]);
+
+        
+        
+    }
+
+    return count;
+}
+// sumIntervals( [[1,4],[7, 10],[3, 5]]);
+sumIntervals( [ [ 1, 20 ], [ 10, 20 ], [ 1, 6 ], [ 16, 19 ], [ 5, 11 ] ]);
+
+
+// 1 20 
+
+// KATA 제목 : Sum of Intervals
