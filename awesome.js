142ae84c0c0bbc8d19dc49a47588a9e5a3f5bc98
unknown <qkrcksdna@naver.com>
2021-01-12 16:07:18 +0900

    풀어볼것

---
+++ awesome.js
@@ -0,0 +1,32 @@
+// Catching Car Mileage Numbers
+
+function isAwesomeNumber(num, awe){
+    //증감
+
+    //증가
+
+    //거울상
+
+    //나머지 모두 0
+
+    //동일 숫자
+
+    let copy = String(num);
+    let numLimit = [num, num +1, num + 2];
+
+    let isAwesome = true;
+
+    //awesome array 에 match
+
+    // 인접(+2 까지) 에 awesome이 있으면 return 1
+
+    // awesome이랑 match면 return 2
+
+    // 위의 두개 모두 아니면 return 0
+
+
+
+}
+
+
+isAwesomeNumber(1337, [1337, 256]);
