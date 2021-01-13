a3365aa125d6ce5cd322daff733315bb87c2ae41
unknown <qkrcksdna@naver.com>
2021-01-13 14:21:18 +0900

    이거는 대단하다...

--- linear.js
+++ linear.js
@@ -2,20 +2,52 @@
 
 function dblLinear(numb){
     let emp = [1];
-    while(emp.length < numb){
-        for(let x of emp){
+    let ind = 0;
+    while(emp.length < 2 * numb){
+        let i = 0;
+        for(; i< emp.length; i++){
+            // console.log(emp[i], emp[numb]);
+            ind = emp[i];
+            if (!emp.includes(2 * emp[i] + 1)){
+                emp.push(2 * emp[i] + 1);
+            }
+            if (!emp.includes(3 * emp[i] + 1)){
+                emp.push(3 * emp[i] + 1);
+            }
             console.log(emp);
-            emp.push(2 * x + 1);
-            emp.push(3 * x + 1);
-            emp = emp.sort((a,b)=> a-b);
-            if(emp.length > numb ){
-                emp.pop();
+            emp = emp.slice(0,i).concat(emp.slice(i).sort((a,b)=> a-b));
+            // console.log(x, 2*x+1 , 3*x+1, emp);
+            console.log(emp);
+            // console.log("길이 : "+emp.length);
+            if(ind > emp[numb]){
                 break;
             }
+            // console.log(emp[i], ind);
+            console.log();
+        }
+        if(ind > emp[numb]){
+            console.log("끝");
+            break;
         }
     }
-    console.log(emp);
-    return emp.pop();
+    // console.log(emp);
+    return emp[numb];
 }
 
-console.log(dblLinear(20));
+console.log(dblLinear(10));
+
+//sort랑, break 조건을 fix하자.
+
+// function dblLinear(n) {
+  
+//     var u = [1], pt2 = 0, pt3 = 0; //two pointer
+    
+//     for(var i = 1;i<=n;i++){
+//       u[i] = Math.min(2* u[pt2] + 1, 3*u[pt3] + 1);
+//       if(u[i] == 2 * u[pt2] + 1) pt2++;
+//       if(u[i] == 3 * u[pt3] + 1) pt3++;
+//     }
+
+//     return u[n];
+  
+// }
