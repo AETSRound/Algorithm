012dfe8cc9716393e8677b5af38051d7c6f07f8d
unknown <qkrcksdna@naver.com>
2021-01-21 17:25:30 +0900

    1000단위 콤마찍는 정규표현식 이해하자.

--- 코딩100제/58번콤마.js
+++ 코딩100제/58번콤마.js
@@ -3,3 +3,5 @@ function comma(numb) {
 }
 
 console.log(comma(154556666234));
+
+// https://mizzo-dev.tistory.com/entry/JavaScript%EC%A0%95%EA%B7%9C%EC%8B%9D-%EC%88%AB%EC%9E%90%EC%97%90-1000%EB%8B%A8%EC%9C%84%EB%A1%9C-%EC%BD%A4%EB%A7%88%EC%89%BC%ED%91%9C-%EA%B5%AC%EB%B6%84%EC%9E%90-%EB%84%A3%EA%B8%B0
